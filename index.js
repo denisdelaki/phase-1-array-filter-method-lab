c9b1d32e3852e126d5941b11ba8d5efbb0c4b8f5
denisdelaki <denisdelaki@gmail.com>
2022-06-10 16:47:39 +0300

    commit tree dea77703d9befa1ef60a05f51af9af4520cb0cc4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (eliminate){
+        return eliminate.toLowerCase().indexOf(name.toLowerCase()) !==-1
+    })
+}
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(function (first) {
+       return first.toLowerCase().indexOf(query.toLowerCase())===0
+    })
+}
+function matchName(drivers, matched) {
+    return drivers.filter(function (matching){
+        return matching.name===matched
+       })
+}
